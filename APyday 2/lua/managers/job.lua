94de6cad1c578632ff8fff7d13b754992caffd12
C@:\EM1NAL <emaperry@stupidcat.uk>
2026-02-25 13:03:14 +0000

    Delete APyday 2 directory

--- APyday 2/lua/managers/job.lua
+++
@@ -1,5 +0,0 @@
--- beautiful. elegant. may cause problems in multiplayer
--- not that it matters as the mod is singleplayer only
-Hooks:PostHook(JobManager, "is_current_job_professional", "apd2_force_projob", function(self)
-  return true
-end)
--- APyday 2/lua/managers/playerinventorygui.lua
+++
@@ -1,6 +0,0 @@
-Hooks:PostHook(PlayerInventoryGui, "init", "apd2_inventorygui", function(self, ws, fullscreen_ws, node)
-  -- this hook is supposed to remove the rightmost column
-  -- from the base inventory (skills/perks/AI/infamy).
-  -- I spent hours trying to figure this out and couldn't
-  -- get something working. that's why this is here.
-end)
