501679030551a1d3672a324e298d753267923f16
C@:\EM1NAL <emaperry@stupidcat.uk>
2026-04-06 23:00:43 +0100

    0.2.4

--- Criminal Dawn/lua/managers/infamy.lua
+++ Criminal Dawn/lua/managers/infamy.lua
@@ -1,5 +1,5 @@
 Hooks:OverrideFunction(InfamyManager, "available", function() return true end)
 
-Hooks:PostHook(InfamyManager, "load", "CrimDawn_InfamyLoad", function(self)
+Hooks:PostHook(InfamyManager, "init", "CrimDawn_InfamyLoad", function(self)
   for _, item in ipairs(tweak_data.infamy.tree) do self:unlock_item(item) end
 end)
--- Criminal Dawn/lua/managers/sidejobevents.lua
+++ Criminal Dawn/lua/managers/sidejobevents.lua
@@ -4,3 +4,10 @@ Hooks:PostHook(SideJobEventManager, "load", "CrimDawn_SideJobEventItems", functi
     for i = 1, #data.rewards do managers.event_jobs:claim_reward(data.id, i) end
   end
 end)
+
+Hooks:PostHook(SideJobEventManager, "reset", "CrimDawn_SideJobEventItems", function(self)
+  for _, data in pairs(tweak_data.event_jobs.challenges) do
+    managers.event_jobs:completed_challenge(data.id)
+    for i = 1, #data.rewards do managers.event_jobs:claim_reward(data.id, i) end
+  end
+end)
