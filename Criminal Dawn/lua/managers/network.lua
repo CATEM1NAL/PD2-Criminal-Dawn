1db001f3ef2f20d224a7933946de2f08e7bd38a1
~/cat/em1/nal/ <emaperry@stupidcat.uk>
2026-08-13 19:46:45 +0100

    Slot names shouldn't break matchmaking

--- Criminal Dawn/lua/managers/network.lua
+++ Criminal Dawn/lua/managers/network.lua
@@ -2,7 +2,7 @@ local FileIdent = "NetworkManager"
 
 -- Assign matchmaking key
 if Global.CrimDawn.data.game.seed then
-  local key = Global.CrimDawn.data.game.seed .. "_" .. Global.CrimDawn.data.game.slot
+  local key = Global.CrimDawn.data.game.seed .. "_" .. Global.CrimDawn.data.game.slot:gsub(" ", "_")
   NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = key
 else
   NetworkMatchMakingSTEAM._BUILD_SEARCH_INTEREST_KEY = "NO SEED FOUND"
